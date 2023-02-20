10ec357b9557abb9cf1cb6627ff3651c9be51882
Mikey <khangnd@rocketstudio.com.vn>
2023-02-21 01:24:28 +0700

    Add GridDebugObject

---
+++ Assets/Scripts/Grid/GridDebugObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GridDebugObject : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro textMeshPro;
+    private GridObject gridObject;
+    public void SetGridObject(GridObject gridObject)
+    {
+        this.gridObject = gridObject;
+        SetTextObject();
+    }
+    private void SetTextObject()
+    {
+        textMeshPro.text = gridObject.ToString();
+    }
+    //private void Update()
+    //{
+    //    textMeshPro.text = gridObject.ToString();
+    //}
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private GridSystem gridSystem;
+    [SerializeField] private Transform gridDebugObjectPrefab;
+    private void Start()
+    {
+        gridSystem =  new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    }
+
+    private void Update()
+    {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
