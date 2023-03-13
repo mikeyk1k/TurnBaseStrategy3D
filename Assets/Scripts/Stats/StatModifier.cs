3a9bb7450b064015ebe3ca2e31a70d2686a9ccfd
Mikey <khangnd@rocketstudio.com.vn>
2023-03-14 01:00:55 +0700

    Test Stats

---
+++ Assets/Scripts/Stats/StatModifier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace StatSystem
+{
+    public enum ModifierOperationType
+    {
+        Additive,
+        Multiplicative,
+        Override
+    }
+
+    public class StatModifier
+    {
+        public Object source { get; set; }
+        public int magnitude { get; set; }
+        public ModifierOperationType type { get; set; }
+    }
+}
